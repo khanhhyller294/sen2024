b107065c08af4f9ce346b1dbe064e9d8f37a8af4
khanhhyller294 <khanhlc294@gmail.com>
2024-10-09 18:40:29 +0700

    sen2024

--- src/a1_12123/Result.java
+++ src/a1_12123/Result.java
@@ -1,53 +1,121 @@
 package engine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result implements Comparable<Result> {
     private Doc doc;
     private List<Match> matches;
-
-    public Result(Doc doc, List<Match> matches) {
-        this.doc = doc;
+    public Result(Doc d, List<Match> matches){
+        this.doc = d;
         this.matches = matches;
     }
 
-    public List<Match> getMatches() {
+    public Doc getDoc() {
+        return doc;
+    }
+
+    public List<Match> getMatches(){
         return matches;
     }
 
-    public Doc getDoc() {
-        return this.doc;
+    public int getMatchCount() {
+        return matches.size();
     }
-
-    public int getTotalFrequency() {
-        return matches.stream().mapToInt(Match::getFreq).sum();
+    public int getTotalFrequency(){
+        int totalfreg = 0;
+        for(Match match : matches){
+            totalfreg += match.getFreq();
+        }
+        return totalfreg;
     }
-
-    public double getAverageFirstIndex() {
-        return matches.stream().mapToInt(Match::getFirstIndex).average().orElse(0.0);
+    public double getAverageFirstIndex(){
+        int totalIndexes = 0;
+        for(Match match : matches) {
+            totalIndexes += match.getFirstIndex();
+        }
+        if (matches.size() > 0) {
+            return (double) totalIndexes / matches.size();
+        }
+        return 0.0;
     }
 
     public String htmlHighlight() {
-        // For simplicity, only working with title words in this implementation
-        StringBuilder html = new StringBuilder();
-        for (Word word : doc.getTitle()) {
-            if (matches.stream().anyMatch(m -> m.getWord().equals(word))) {
-                html.append("<u>").append(word.getText()).append("</u> ");
+        StringBuilder resultHtml = new StringBuilder();
+        List<Word> titleWords = doc.getTitle();
+        List<Word> bodyWords = doc.getBody();
+
+        boolean inTitle = true;
+
+        resultHtml.append("<h3>"); // Add the opening <h3> tag at the beginning
+
+        for (Word word : titleWords) {
+            String htmlTag = getHtmlTag(word, inTitle);
+
+            if (!htmlTag.isEmpty()) {
+                if(!word.getPrefix().isEmpty())  resultHtml.append(word.getPrefix().trim());
+                resultHtml.append(htmlTag);
+                resultHtml.append(word.getText());
+                resultHtml.append("</" + htmlTag.substring(1) + "");
+                if(!word.getSuffix().isEmpty())  resultHtml.append(word.getSuffix().trim());
             } else {
-                html.append(word.getText()).append(" ");
+                resultHtml.append(word);
             }
+            if(titleWords.indexOf(word) != titleWords.size() -1)
+                resultHtml.append(" ");
         }
-        return html.toString().trim();
+        resultHtml.append("</h3>"); // Close the <h3> tag for the title
+
+        inTitle = false;
+        resultHtml.append("<p>"); // Open <p> tag for the body section
+
+        for (Word word : bodyWords) {
+            String htmlTag = getHtmlTag(word, inTitle);
+            if (!htmlTag.isEmpty()) {
+                if(!word.getPrefix().isEmpty())  resultHtml.append(word.getPrefix().trim());
+                resultHtml.append(htmlTag);
+                resultHtml.append(word.getText());
+                resultHtml.append("</" + htmlTag.substring(1) + "");
+                if(!word.getSuffix().isEmpty()) {
+                    resultHtml.append(word.getSuffix().trim());
+                }
+            } else {
+                resultHtml.append(word);
+            }
+            if(bodyWords.indexOf(word) != bodyWords.size() -1)
+                resultHtml.append(" ");
+        }
+        // Close the <p> tag for the body section
+        resultHtml.append("</p>");
+        String result =  resultHtml.toString();
+        result = result.replaceAll(" +</p>", "</p>");
+        return result;
     }
 
+    private String getHtmlTag(Word word, boolean inTitle) {
+        for (Match match : matches) {
+            if (match.getWord().equals(word)) {
+                if (inTitle) {
+                    return "<u>";
+                } else {
+                    return "<b>";
+                }
+            }
+        }
+
+        // If the word doesn't match any term in matches, return the word itself
+        return "";
+    }
+
+
     @Override
-    public int compareTo(Result other) {
-        int matchCountComparison = Integer.compare(other.getMatches().size(), this.getMatches().size());
-        if (matchCountComparison != 0) return matchCountComparison;
-
-        int totalFreqComparison = Integer.compare(other.getTotalFrequency(), this.getTotalFrequency());
-        if (totalFreqComparison != 0) return totalFreqComparison;
-
-        return Double.compare(this.getAverageFirstIndex(), other.getAverageFirstIndex());
+    public int compareTo(Result otherResult){
+        if(this.getMatchCount() != otherResult.getMatchCount()){
+            return Integer.compare(otherResult.getMatchCount(),this.getMatchCount());
+        } else if (this.getTotalFrequency() != otherResult.getTotalFrequency()) {
+            return Integer.compare(otherResult.getTotalFrequency(),this.getTotalFrequency());
+        } else {
+            return Double.compare(this.getAverageFirstIndex(),otherResult.getAverageFirstIndex());
+        }
     }
 }
